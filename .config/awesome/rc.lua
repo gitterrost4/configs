7bf43013032a4ed515da454315b5915ce8adc942
Paul Kramer <gitterrost4@gmail.com>
2012-11-25 12:30:34 +0100

    when opening mutt, the left panel should be active

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -291,7 +291,7 @@ globalkeys = awful.util.table.join(
     -- Home Commands
     awful.key({ modkey, "Mod1"    }, "j", function () awful.util.spawn("jdownloader") end),
     awful.key({ modkey, "Mod1"    }, "t", function () 
-      awful.util.spawn("tmux new-window \"mutt\"\\; split-window -h \"mutt -F ~/.work.muttrc\"")
+      awful.util.spawn("tmux new-window \"mutt\"\\; split-window -h \"mutt -F ~/.work.muttrc\"; select-pane -L")
     end),
     awful.key({ modkey, "Mod1"    }, "m", function () awful.util.spawn("minecraft") end),
     awful.key({                   }, "XF86MonBrightnessUp", function () awful.util.spawn("xbacklight =100") end),
