748d0515a91846d604036dad2df7ba0be8acbb8e
Paul Kramer <paul.kramer@dbfuhrpark.com>
2012-12-03 06:31:32 +0100

    awesome config luakit

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -432,6 +432,8 @@ else
         { rule = { class = "gimp" },
         properties = { floating = true } },
         -- Set Firefox to always map on tags number 2 of screen 1.
+        { rule = { class = "luakit" },
+        properties = { tag = tags[1][1] } },
         { rule = { class = "Firefox" },
         properties = { tag = tags[1][1] } },
         { rule = { class = "Chromium" },
